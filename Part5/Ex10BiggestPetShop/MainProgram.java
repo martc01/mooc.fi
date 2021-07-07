715bdf51abe6338d7f8f5f3fb2257d9f28f3c9ac
mart <69021031+martc01@users.noreply.github.com>
2021-07-07 17:38:59 -0300

    Add files via upload

---
+++ Part5/Ex10BiggestPetShop/MainProgram.java
@@ -0,0 +1,10 @@
+package Part5.Ex10BiggestPetShop;
+
+public class MainProgram {
+    public static void main(String[] args){
+        Pet lucy = new Pet("Lucy", "golden retriever");
+        Person leo = new Person("Leo", lucy);
+
+        System.out.println(leo);
+    }
+}
